3d9f56b9308d927b2d75c83e2ef62a179e5dd769
syn52 <iloveDW152>
2018-06-09 09:52:56 -0700

    Initial commit

---
+++ src/src/Game.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.Scanner;
+
+/**
+ * A 21 game implementation. Starter file
+ */
+public class Game {
+
+    public static void main(String[] args) {
+
+        int numSticks = 21;
+        System.out.println("Would you like to go first? (Y/N)");
+        Scanner input = new Scanner(System.in);
+        String goFirst = input.nextLine();
+        Scanner take = new Scanner(System.in);
+        int numToTake = 0;
+
+        while (numSticks > 0) {
+
+
+            // Your code goes here
+
+
+        }
+
+    }
+
+}
